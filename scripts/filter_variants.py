00f5e79a54993ddcd6e93634ec279e42b65b45b2
NavarroJ <59834493+NavarroJ@users.noreply.github.com>
2020-04-29 18:44:54 -0400

    changed cutoff to 0.03

--- scripts/filter_variants.py
+++ scripts/filter_variants.py
@@ -6,5 +6,5 @@ for file in os.listdir("/Users/johnnavarro/AS_variants_list"):
             outfile = file.split(".")[0] + "filtered.txt"
             with open(outfile, "w+") as fout:
                 for line in fin:
-                    if float(line.split("\t")[5]) > 0.01:
+                    if float(line.split("\t")[5]) > 0.03:
                         fout.write(line)
